9d4615479ab00feb65cb682392875cfd7114f17d
Unknown <enzo.billis@viacesi.fr>
2017-06-16 19:27:25 +0200

    Connecting complete

--- BoulderDash/model/src/main/java/model/dao/BoulderDashBDDConnector.java
+++ BoulderDash/model/src/main/java/model/dao/BoulderDashBDDConnector.java
@@ -21,10 +21,10 @@ final class BoulderDashBDDConnector {
     private static String                  user     = "root";
 
     /** The password. */
-    private static String                  password = "";
+    private static String                  password = "javalehutt";
 
     /** The url. */
-    private static String                  url      = "jdbc:mysql://localhost/boulderdash?useSSL=false&serverTimezone=UTC";
+    private static String                  url      = "jdbc:mysql://10.176.130.24/boulderdash";
 
     /** The connection. */
     private Connection                     connection;
