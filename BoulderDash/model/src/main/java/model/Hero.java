b5c2e8a6eacc3825fbb5dfa1ed282b5b5e9b8302
Gabgabga <gabriel.lesourd@viacesi.fr>
2017-06-19 17:11:00 +0200

    Add Hero and HeroTest

---
+++ BoulderDash/model/src/main/java/model/Hero.java
@@ -0,0 +1,83 @@
+package model;
+
+import java.awt.Image;
+import java.util.Observable;
+
+public class Hero extends Observable {
+	
+	private int direction;
+	private int posX;
+	private int posY;
+	private int diamonds;
+	private boolean aLive;
+	private Image image;
+	
+	public Hero(){
+		this.posX=1;
+		this.posY= 1 ;
+		this.direction=3;
+		this.aLive= true;
+	}
+	public void walkOn(){
+		
+	}
+	
+	public void move(){
+		
+	}
+	
+	public void pushStone(){
+		
+	}
+
+	public int getDirection() {
+		return direction;
+	}
+
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getDiamonds() {
+		return diamonds;
+	}
+
+	public void setDiamonds(int diamonds) {
+		this.diamonds = diamonds;
+	}
+
+	public boolean isaLive() {
+		return aLive;
+	}
+
+	public void setaLive(boolean aLive) {
+		this.aLive = aLive;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	
+
+}
