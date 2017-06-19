5c41f38ac5a323b583972e35264ead6fc4a16220
Gabgabga <gabriel.lesourd@viacesi.fr>
2017-06-19 17:50:31 +0200

    Add Diamond and DiamondTest and DecorWithExplosion

---
+++ BoulderDash/model/src/main/java/model/Diamond.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.Image;
+
+public class Diamond extends DecorWithExplosion {
+	
+	public void addPoint(){
+		
+		
+	}
+	
+	public Diamond(int posX, int posY, Image image){
+		
+		
+		this.setPosX(posX);
+		this.setPosY(posY);
+		this.setImage(image);
+		this.blocker = false;
+		this.breakable = true;
+		
+		}
+
+}
