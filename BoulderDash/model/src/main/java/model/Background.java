6f8ea6ac1759567f119f557a78dfb19d9fef15e4
ArthurDelamare <arthur.delamare1@viacesi.fr>
2017-06-19 18:33:10 +0200

    ADD Background and BackgroundTest

---
+++ BoulderDash/model/src/main/java/model/Background.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.awt.Image;
+
+public class Background extends AbstractDecor{
+	
+	public Background(int posX, int posY, Image image){
+		this.setPosX(posX);
+		this.setPosY(posY);
+		this.setImage(image);
+		this.blocker = false;
+		this.breakable = false;
+	}
+
+}
