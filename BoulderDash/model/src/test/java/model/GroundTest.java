22a7ba060e8f92759888129575cd27b496e4522f
ArthurDelamare <arthur.delamare1@viacesi.fr>
2017-06-19 16:11:32 +0200

    ADD model class : Ground AND GroundTest

---
+++ BoulderDash/model/src/test/java/model/GroundTest.java
@@ -0,0 +1,74 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GroundTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBreaking() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPosX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPosY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBreakable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBlocker() {
+		fail("Not yet implemented");
+	}
+
+}
