165b4ee33adfd7eb5f6df70cf9c12b53c72a63d0
ArthurDelamare <arthur.delamare1@viacesi.fr>
2017-06-20 16:10:24 +0200

    UPDATE Interface Mapeable + Class Ground AND GroundTest

--- BoulderDash/model/src/main/java/model/Ground.java
+++ BoulderDash/model/src/main/java/model/Ground.java
@@ -4,13 +4,14 @@ import java.awt.Image;
 
 public class Ground extends AbstractDecor{
 	
-	Ground(int posX, int posY, Image image){
+	Ground(int posX, int posY, Image image, int mapTable[][]){
 		
 		this.setPosX(posX);
 		this.setPosY(posY);
 		this.setImage(image);
 		this.blocker = false;
 		this.breakable = true;
+		this.mapTable = mapTable;
 		
 	}
 	
