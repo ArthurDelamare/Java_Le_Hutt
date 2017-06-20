c57706c7c937d4a3ffa75b013341e5384ede75d5
Gabgabga <gabriel.lesourd@viacesi.fr>
2017-06-20 16:07:34 +0200

    add ViewTest

---
+++ BoulderDash/view/src/test/java/view/ViewTest.java
@@ -0,0 +1,49 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetBulderFrame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBulderFrame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+}
