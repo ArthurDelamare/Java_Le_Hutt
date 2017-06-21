7c6c047afa61afffab8c31d95fa7a3696f20ea5d
Enzo BILLIS <enzo.billis@viacesi.fr>
2017-06-21 10:14:49 +0200

    Update SpriteSheet - Actually in test

--- BoulderDash/model/src/main/java/model/SpriteSheet.java
+++ BoulderDash/model/src/main/java/model/SpriteSheet.java
@@ -1,35 +1,50 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class SpriteSheet {
-public BufferedImage sheet;
 	
-	public SpriteSheet(BufferedImage sheet){
-		this.sheet = sheet;
-	}
-	
-	public BufferedImage crop(int x, int y, int width, int height){
-		return sheet.getSubimage(x, y, width, height);
-	}
-	
-private static final int width = 16, height = 16;
-	
-	public static BufferedImage testimage, player, terrevide, terrepleine, rocher, mur, diamant, mob;
-	
-	public static void init(){
-		//testimage = AbstractDecor.loadImage("/textures/74336.png");
-		//g.setColor(Color.red);
-		//g.fillRect(O, O, 10, 10);
-		SpriteSheet sheet = new SpriteSheet(AbstractDecor.loadImage("/textures/74336.png"));
-		SpriteSheet sheet2 = new SpriteSheet(AbstractDecor.loadImage("/textures/74359.png"));
+	public BufferedImage spriteSheet(int typeOfDecors){
 		
-		player = sheet.crop(0, 0, width, height);
-		terrevide = sheet2.crop(32, 0, width, height);
-		terrepleine = sheet2.crop(16, 0, width, height);
-		rocher = sheet2.crop(48, 0, width, height);
-		mur = sheet2.crop(0, 0, width, height);
-		diamant = sheet2.crop(64, 0, width, height);
-		mob = sheet2.crop(144, 0, width, height);
-}
+		BufferedImage imageDecors = null;
+		BufferedImage imageHero = null;
+		BufferedImage finalImage = null;
+	
+		try {
+			imageDecors = ImageIO.read(new File("../textures/decors.png"));
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		try {
+			imageHero = ImageIO.read(new File("../textures/personnage.png"));
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		
+		switch(typeOfDecors){
+		case 0:
+			finalImage = imageDecors.getSubimage(0,0,16,16);
+		case 1:
+			finalImage = imageDecors.getSubimage(16,0,16,16);
+		case 2:
+			finalImage = imageDecors.getSubimage(48,0,16,16);
+		case 3:
+			finalImage = imageDecors.getSubimage(64,0,16,16);
+		case 4:
+			finalImage = imageDecors.getSubimage(32,0,16,16);
+		case 5:
+			finalImage = imageDecors.getSubimage(176,0,16,16);
+		case 6:
+			finalImage = imageHero.getSubimage(0,0,16,16);
+		}
+		return finalImage;
+		
+	}
+	
 }
