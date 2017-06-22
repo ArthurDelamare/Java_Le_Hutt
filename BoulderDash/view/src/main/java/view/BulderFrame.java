eb9c66cd8b60db7a76fd57dfa6ad938ce9848026
Enzo BILLIS <enzo.billis@viacesi.fr>
2017-06-22 10:35:13 +0200

    Fix last Merge

--- BoulderDash/view/src/main/java/view/BulderFrame.java
+++ BoulderDash/view/src/main/java/view/BulderFrame.java
@@ -1,84 +1,1 @@
-package view;
-
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.JFrame;
-
-import controller.IMovementController;
-
-
-public class BulderFrame extends JFrame implements KeyListener, IBulderFrame{
-	
-	private BulderPanel bulderPanel;
-	private IMovementController movementController;
-	
-	public BulderFrame(){
-			
-		this.bulderPanel = new BulderPanel();
-		
-		this.setTitle("BulderDash");
-		this.setSize(500, 500);
-		this.setLocationRelativeTo(null);
-		this.setContentPane(bulderPanel);
-		
-		this.addKeyListener(this);
-		this.setVisible(true);
-		Image icon = Toolkit.getDefaultToolkit().getImage("iconjeu.png");
-	    this.setIconImage(icon);
-	}
-
-	/* (non-Javadoc)
-	 * @see view.IBulderFrame#getPanel()
-	 */
-	@Override
-	public IBulderPanel getPanel() {
-		return bulderPanel;
-	}
-
-	/* (non-Javadoc)
-	 * @see view.IBulderFrame#setPanel(view.BulderPanel)
-	 */
-	@Override
-	public void setPanel(IBulderPanel panel) {
-		this.bulderPanel = (BulderPanel) panel;
-	}
-
-	/* (non-Javadoc)
-	 * @see view.IBulderFrame#keyPressed(java.awt.event.KeyEvent)
-	 */
-	@Override
-	public void keyPressed(KeyEvent e) {
-		System.out.println(e.getKeyCode());
-	}
-
-	/* (non-Javadoc)
-	 * @see view.IBulderFrame#keyReleased(java.awt.event.KeyEvent)
-	 */
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see view.IBulderFrame#keyTyped(java.awt.event.KeyEvent)
-	 */
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	/* (non-Javadoc)
-	 * @see view.IBulderFrame#setMovementController(controller.MovementController)
-	 */
-	@Override
-	public void setMovementController(IMovementController movementController){
-		this.movementController = movementController;
-	}
-	
-}
-
+package view;import java.awt.event.KeyEvent;import java.awt.event.KeyListener;import javax.swing.JFrame;import controller.IMovementController;public class BulderFrame extends JFrame implements KeyListener, IBulderFrame{		private BulderPanel bulderPanel;	private IMovementController movementController;		public BulderFrame(BulderPanel bulderPanel){					this.bulderPanel = bulderPanel;				this.setTitle("BulderDash");		this.setSize(650, 645);		this.setLocationRelativeTo(null);		this.setContentPane(bulderPanel);				this.addKeyListener(this);				this.setVisible(true);			}	/* (non-Javadoc)	 * @see view.IBulderFrame#getPanel()	 */	@Override	public IBulderPanel getPanel() {		return bulderPanel;	}	/* (non-Javadoc)	 * @see view.IBulderFrame#setPanel(view.BulderPanel)	 */	@Override	public void setPanel(IBulderPanel panel) {		this.bulderPanel = (BulderPanel) panel;	}	/* (non-Javadoc)	 * @see view.IBulderFrame#keyPressed(java.awt.event.KeyEvent)	 */	@Override	public void keyPressed(KeyEvent e) {		System.out.println(e.getKeyCode());	}	/* (non-Javadoc)	 * @see view.IBulderFrame#keyReleased(java.awt.event.KeyEvent)	 */	@Override	public void keyReleased(KeyEvent arg0) {		// TODO Auto-generated method stub			}	/* (non-Javadoc)	 * @see view.IBulderFrame#keyTyped(java.awt.event.KeyEvent)	 */	@Override	public void keyTyped(KeyEvent arg0) {		// TODO Auto-generated method stub			}		/* (non-Javadoc)	 * @see view.IBulderFrame#setMovementController(controller.MovementController)	 */	@Override	public void setMovementController(IMovementController movementController){		this.movementController = movementController;	}	}
