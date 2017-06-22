3674c66abe46c96360d596b251a6c9d7d20a9564
ArthurDelamare <arthur.delamare1@viacesi.fr>
2017-06-22 22:41:35 +0200

    ADD variable movement keyboard

--- BoulderDash/controller/src/main/java/controller/MovementController.java
+++ BoulderDash/controller/src/main/java/controller/MovementController.java
@@ -12,8 +12,13 @@ public class MovementController implements IMovementController {
 	
 	
 	@Override
-	public void askMoveHero(){
+	public void askMoveHero(int keyCode){
 		System.out.println("Mouvement détecté.");
+		
+		if(keyCode == 90) this.hero.setDirection(1);
+		else if(keyCode == 68) this.hero.setDirection(2);
+		else if(keyCode == 83) this.hero.setDirection(3);
+		else if(keyCode == 81) this.hero.setDirection(4);
 		
 		this.hero.move();
 		this.hero.getMap().getMapObjects().clear();
