a389ef44e1db26a719db6966017d845e66f1e1db
Enzo BILLIS <enzo.billis@viacesi.fr>
2017-06-23 11:20:14 +0200

    Hero don't pass the mob right now

--- BoulderDash/controller/src/main/java/controller/MovementController.java
+++ BoulderDash/controller/src/main/java/controller/MovementController.java
@@ -23,19 +23,19 @@ public class MovementController implements IMovementController {
 		
 		
 		
-		if(keyCode == 90 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())-1) != 0)) {
+		if(keyCode == 90 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())-1) != 0) && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())-1) != 5)) {
 			this.hero.setDirection(1);
 			this.callMovement();
 		}
-		else if(keyCode == 68 && (this.hero.getMap().getCellTable((this.hero.getPosX())+1, (this.hero.getPosY())) != 0) ) {
+		else if(keyCode == 68 && (this.hero.getMap().getCellTable((this.hero.getPosX())+1, (this.hero.getPosY())) != 0) && (this.hero.getMap().getCellTable((this.hero.getPosX())+1, (this.hero.getPosY())) != 5) ) {
 			this.hero.setDirection(2);
 			this.callMovement();
 		}
-		else if(keyCode == 83 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())+1) != 0)) {
+		else if(keyCode == 83 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())+1) != 0) && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())+1) != 5)) {
 			this.hero.setDirection(3);
 			this.callMovement();
 		}
-		else if(keyCode == 81 && (this.hero.getMap().getCellTable((this.hero.getPosX())-1, (this.hero.getPosY())) != 0) ) {
+		else if(keyCode == 81 && (this.hero.getMap().getCellTable((this.hero.getPosX())-1, (this.hero.getPosY())) != 0) && (this.hero.getMap().getCellTable((this.hero.getPosX())-1, (this.hero.getPosY())) != 5)) {
 			this.hero.setDirection(4);
 			this.callMovement();
 		}
