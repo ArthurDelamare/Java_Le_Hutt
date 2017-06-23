2e2d184a628bb65f4e4211363a4b39c06f5b4aad
Enzo BILLIS <enzo.billis@viacesi.fr>
2017-06-23 11:42:02 +0200

    Hero now eat diamond and count them

--- BoulderDash/controller/src/main/java/controller/MovementController.java
+++ BoulderDash/controller/src/main/java/controller/MovementController.java
@@ -10,11 +10,34 @@ public class MovementController implements IMovementController {
 		this.hero = hero;
 	}
 	
-	private void callMovement(){
+	private void callMovement(int sens){
+			if (this.hero.getMap().getCellTable((this.hero.getPosX()-1), (this.hero.getPosY())) == 3 && sens == 4){
+			
+			this.hero.setDiamonds(this.hero.getDiamonds()+1);
+			System.out.println("nombre de diamands : " + this.hero.getDiamonds());
+		}
+			if (this.hero.getMap().getCellTable((this.hero.getPosX()), (this.hero.getPosY()+1)) == 3 && sens == 3){
+				
+				this.hero.setDiamonds(this.hero.getDiamonds()+1);
+				System.out.println("nombre de diamands : " + this.hero.getDiamonds());
+			}
+			if (this.hero.getMap().getCellTable((this.hero.getPosX()+1), (this.hero.getPosY())) == 3 && sens == 2){
+				
+				this.hero.setDiamonds(this.hero.getDiamonds()+1);
+				System.out.println("nombre de diamands : " + this.hero.getDiamonds());
+			}
+			if (this.hero.getMap().getCellTable((this.hero.getPosX()), (this.hero.getPosY()-1)) == 3 && sens == 1){
+				
+				this.hero.setDiamonds(this.hero.getDiamonds()+1);
+				System.out.println("nombre de diamands : " + this.hero.getDiamonds());
+			}
+			
+		this.hero.setDirection(sens);
 		this.hero.move();
 		this.hero.getMap().getMapObjects().clear();
 		this.hero.getMap().fillMapObjects();
 		this.hero.updatePanel();
+		
 	}
 	
 	
@@ -24,20 +47,16 @@ public class MovementController implements IMovementController {
 		
 		
 		if(keyCode == 90 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())-1) != 0) && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())-1) != 5)) {
-			this.hero.setDirection(1);
-			this.callMovement();
+			this.callMovement(1);
 		}
 		else if(keyCode == 68 && (this.hero.getMap().getCellTable((this.hero.getPosX())+1, (this.hero.getPosY())) != 0) && (this.hero.getMap().getCellTable((this.hero.getPosX())+1, (this.hero.getPosY())) != 5) ) {
-			this.hero.setDirection(2);
-			this.callMovement();
+			this.callMovement(2);
 		}
 		else if(keyCode == 83 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())+1) != 0) && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())+1) != 5)) {
-			this.hero.setDirection(3);
-			this.callMovement();
+			this.callMovement(3);
 		}
 		else if(keyCode == 81 && (this.hero.getMap().getCellTable((this.hero.getPosX())-1, (this.hero.getPosY())) != 0) && (this.hero.getMap().getCellTable((this.hero.getPosX())-1, (this.hero.getPosY())) != 5)) {
-			this.hero.setDirection(4);
-			this.callMovement();
+			this.callMovement(4);
 		}
 		
 		
