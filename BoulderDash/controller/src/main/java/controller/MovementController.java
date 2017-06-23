7bf5d47033d203aac6862710b5beec2249d6f94c
Enzo BILLIS <enzo.billis@viacesi.fr>
2017-06-23 11:15:14 +0200

    Hero don't pass the walls right now

--- BoulderDash/controller/src/main/java/controller/MovementController.java
+++ BoulderDash/controller/src/main/java/controller/MovementController.java
@@ -10,19 +10,36 @@ public class MovementController implements IMovementController {
 		this.hero = hero;
 	}
 	
-	
-	@Override
-	public void askMoveHero(int keyCode){
-		
-		if(keyCode == 90) this.hero.setDirection(1);
-		else if(keyCode == 68) this.hero.setDirection(2);
-		else if(keyCode == 83) this.hero.setDirection(3);
-		else if(keyCode == 81) this.hero.setDirection(4);
-		
+	private void callMovement(){
 		this.hero.move();
 		this.hero.getMap().getMapObjects().clear();
 		this.hero.getMap().fillMapObjects();
 		this.hero.updatePanel();
+	}
+	
+	
+	@Override
+	public void askMoveHero(int keyCode){
+		
+		
+		
+		if(keyCode == 90 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())-1) != 0)) {
+			this.hero.setDirection(1);
+			this.callMovement();
+		}
+		else if(keyCode == 68 && (this.hero.getMap().getCellTable((this.hero.getPosX())+1, (this.hero.getPosY())) != 0) ) {
+			this.hero.setDirection(2);
+			this.callMovement();
+		}
+		else if(keyCode == 83 && (this.hero.getMap().getCellTable(this.hero.getPosX(), (this.hero.getPosY())+1) != 0)) {
+			this.hero.setDirection(3);
+			this.callMovement();
+		}
+		else if(keyCode == 81 && (this.hero.getMap().getCellTable((this.hero.getPosX())-1, (this.hero.getPosY())) != 0) ) {
+			this.hero.setDirection(4);
+			this.callMovement();
+		}
+		
 		
 	}
 }
--- BoulderDash/view/src/main/java/view/GraphicsBuilder.java
+++ BoulderDash/view/src/main/java/view/GraphicsBuilder.java
@@ -13,7 +13,7 @@ public class GraphicsBuilder implements IGraphicsBuilder {
 	}
 	
 	@Override
-	public void applyDrawTographics(Graphics g){		g.setColor(Color.WHITE);		g.fillRect(0, 0, 600, 600);				int tempPosX = 0;		int tempPosY = 0;		
+	public void applyDrawTographics(Graphics g){		//g.setColor(Color.WHITE);		//g.fillRect(0, 0, 600, 600);				int tempPosX = 0;		int tempPosY = 0;		
 		for(int i = 0; i<this.map.getMapObjects().size(); i++){
 			g.drawImage(this.map.getMapObjects().get(i).getImage(), (this.map.getMapObjects().get(i).getPosX())*32 - this.map.getMapObjects().get(0).getPosX()*32, (this.map.getMapObjects().get(i).getPosY())*32 - this.map.getMapObjects().get(0).getPosY()*32, 32, 32, null);						if (this.map.getMapObjects().get(i).getImage() == this.map.getHero().getImage()){				tempPosX = this.map.getMapObjects().get(i).getPosX();				tempPosY = this.map.getMapObjects().get(i).getPosY();			}
 		}		
